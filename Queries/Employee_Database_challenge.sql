a0690a62c10a934fd8689a5e809c81d66bd60438
Spring Wedlund <wedlundspring@gmail.com>
2022-01-28 16:38:49 -0800

    updating EmployeeDB.png

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,54 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Deliverable 1
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+SELECT  e.emp_no,
+        e.first_name,
+        e.last_name,
+        t.title,
+        t.from_date,
+        t.to_date
+INTO retirement_titles
+FROM employees as e 
+INNER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp,_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+-- Create a Unique Titles table using the INTO clause
+INTO unique_titles
+FROM retirement_titles AS rt 
+ORDER BY emp_no ASC, to_date DESC;
+SELECT * FROM unique_titles;
+
+--Create a Retiring Titles table
+SELECT COUNT (ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut 
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+SELECT * FROM retiring_titles;
+
+-- Deliverable 2
+-- The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title 
+INTO mentorship_elibility
+FROM employees as e 
+LEFT OUTER JOIN titles as t 
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+SELECT * FROM mentorship_elibility;
