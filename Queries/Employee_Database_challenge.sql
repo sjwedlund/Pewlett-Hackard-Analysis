c4869bb1c890d2e0378fccb9d8954d1d2764e114
Spring Wedlund <wedlundspring@gmail.com>
2022-02-28 14:30:21 -0800

    updating Employee_Database_challenge.sql, adding CSV files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,21 +34,23 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 SELECT * FROM retiring_titles;
 
--- Deliverable 2
--- The Employees Eligible for the Mentorship Program
-SELECT DISTINCT ON(e.emp_no) e.emp_no,
-    e.first_name,
-    e.last_name,
-    e.birth_date,
-    de.from_date,
-    de.to_date,
-    t.title 
-INTO mentorship_elibility
-FROM employees as e 
-LEFT OUTER JOIN titles as t 
-ON (e.emp_no = de.emp_no)
-LEFT OUTER JOIN titles as t 
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
-SELECT * FROM mentorship_elibility;
+--Deliverable 2
+--Mentorship Eligibility table
+
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+--DISTINCT ON(emp_no)
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles AS ti
+				ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
