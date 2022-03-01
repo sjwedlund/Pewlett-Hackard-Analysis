338cc9fd5b0ffc0b53c70278da9a0eaf2052d836
Spring Wedlund <wedlundspring@gmail.com>
2022-02-28 19:51:11 -0800

    updating files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -43,14 +43,14 @@ SELECT DISTINCT ON (e.emp_no)e.emp_no,
 	e.birth_date,
 	de.from_date,
 	de.to_date,
-	ti.title
+	t.title
 --DISTINCT ON(emp_no)
 INTO mentorship_eligibility
 FROM employees AS e
 	INNER JOIN dept_emp AS de
 		ON (e.emp_no = de.emp_no)
-			INNER JOIN titles AS ti
-				ON (e.emp_no = ti.emp_no)
+			INNER JOIN titles AS t
+				ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY emp_no;
