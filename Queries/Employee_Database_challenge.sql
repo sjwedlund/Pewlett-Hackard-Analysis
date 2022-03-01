4188786d0fb5e1a0d0219d6102d0562c495504be
Spring Wedlund <wedlundspring@gmail.com>
2022-02-28 19:53:58 -0800

    updating file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,7 +11,7 @@ FROM employees as e
 INNER JOIN titles as t 
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-order by e.emp_no;
+ORDER BY e.emp_no;
 
 -- Use Distinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp,_no,
@@ -47,10 +47,10 @@ SELECT DISTINCT ON (e.emp_no)e.emp_no,
 --DISTINCT ON(emp_no)
 INTO mentorship_eligibility
 FROM employees AS e
-	INNER JOIN dept_emp AS de
-		ON (e.emp_no = de.emp_no)
-			INNER JOIN titles AS t
-				ON (e.emp_no = t.emp_no)
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY emp_no;
